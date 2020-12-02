dd4d0ecd1b9dca70d4009ee306894985103fbfed
Alvaro012 <45605035+Alvaro012@users.noreply.github.com>
2020-12-02 16:47:28 +0100

    Add files via upload

---
+++ ejercicio2.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <iostream>
+#include <errno.h>
+
+using namespace std;
+
+int main(){
+	if(setuid(2)== -1){
+                cout<<"Error:"<<strerror(errno)<<endl;
+                cout<< "N?mero de error:"<< *strerror(errno)<<endl;
+        }
+	return 1;
+}
