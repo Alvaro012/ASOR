56c6df20a5e0c21fff9c1009d7b4afde62d3ae67
Alvaro012 <45605035+Alvaro012@users.noreply.github.com>
2020-12-05 11:50:45 +0100

    Add files via upload

---
+++ ejercicio11.cpp
@@ -0,0 +1,16 @@
+#include <sys/types.h>
+#include <unistd.h>
+#include <iostream>
+#include <sys/utsname.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+using namespace std;
+
+int main(){
+	int i;
+	i=system("man date");
+        return 1;
+}
+
+
