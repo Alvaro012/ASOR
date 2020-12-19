435d52fb5dc5a3031e7399d96254e45f58bd1405
Alvaro012 <45605035+Alvaro012@users.noreply.github.com>
2020-12-19 16:49:34 +0100

    Add files via upload

---
+++ ejercicio 4.cpp
@@ -0,0 +1,14 @@
+#include <sys/types.h>
+#include <unistd.h>
+#include <sys/utsname.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+
+using namespace std;
+
+int main(){
+	chmod("home/cursoredes/directorio",S_IRUSR| S_IWUSR|S_IRGRP| S_IWGRP| S$
+        return 1;
+}
+
