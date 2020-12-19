e8863829cf6bf5a38c19ca88bb92d248560805c1
Alvaro012 <45605035+Alvaro012@users.noreply.github.com>
2020-12-19 12:25:59 +0100

    Add files via upload

---
+++ ejercicio1.cpp
@@ -0,0 +1,14 @@
+#include <sys/types.h>
+#include <unistd.h>
+#include <sys/utsname.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+using namespace std;
+
+int main(){
+	int i;
+	i=system("man ls");
+        return 1;
+}
+
