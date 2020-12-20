f9fca7d1552ad9e559a4838b2a2af7ae62650507
Alvaro012 <45605035+Alvaro012@users.noreply.github.com>
2020-12-20 19:41:41 +0100

    Update ejercicio 3.cpp

--- ejercicio 3.cpp
+++ ejercicio 3.cpp
@@ -8,7 +8,9 @@
 using namespace std;
 
 int main(){
+	//chmod 540 fichero
 	chmod("home/cursoredes/fichero",S_IRUSR| S_IXUSR|S_IRGRP);
+	//chmod u+rx,g+r-wx,o-wxr fichero
 	chmod("home/cursoredes/fichero",S_IRUSR| S_IXUSR|S_IWGRP| S_IXGRP| S_IROTH|S_IWOTH|S_IXOTH);
         return 1;
 }
