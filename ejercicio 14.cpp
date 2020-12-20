b0c1f47b76ed671796971ab2dbd279873aeb402c
Alvaro012 <45605035+Alvaro012@users.noreply.github.com>
2020-12-20 18:30:30 +0100

    Add files via upload

---
+++ ejercicio 14.cpp
@@ -0,0 +1,20 @@
+#include <sys/types.h>
+#include <unistd.h>
+#include <sys/utsname.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <time.h>
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	system("nano /proc/locks");
+ return 1;
+
+}
+
+
+
