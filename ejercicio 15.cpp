15cbb5581f5497f64681cf95e28c1453ceac05d8
Alvaro012 <45605035+Alvaro012@users.noreply.github.com>
2020-12-20 19:00:00 +0100

    Update ejercicio 15.cpp

--- ejercicio 15.cpp
+++ ejercicio 15.cpp
@@ -1,12 +1,9 @@
 #include <sys/types.h>
 #include <unistd.h>
+#include <iostream>
 #include <sys/utsname.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <time.h>
-#include <iostream>
 #include <time.h>
 #include <sys/time.h>
 
@@ -17,21 +14,10 @@ int main(){
         struct tm *timeinfo;
         char buffer [80];
         rawtime = time(0);
-	timeinfo = localtime(&rawtime);
+        timeinfo = localtime(&rawtime);
         setlocale(LC_ALL,NULL);
-        strftime (buffer,80," %r",timeinfo);
-        int fd = open("fichero",O_WRONLY | O_APPEND);
-        if (lockf(fd,F_ULOCK,0)== -1){
-                cout<<"Fichero no está lock"<<endl;
-                lockf(fd,F_LOCK,0);
-                cout<<buffer<<endl;
-                sleep(30);
-                lockf(fd,F_ULOCK,0);	
+        strftime (buffer,80,"%A,%d de %B de %Y, %H:%M",timeinfo);
+  	cout<<"Fecha es: "<<buffer<<endl;
+        return 1;
 }
-else{
-     	cout<<"Fichero está lock"<<endl;
-}
- return 1;
-}
-
 
