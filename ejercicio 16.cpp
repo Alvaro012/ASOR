41d8867ed2acc3425ebfb82e838732a40b098ae4
Alvaro012 <45605035+Alvaro012@users.noreply.github.com>
2020-12-20 19:02:07 +0100

    Add files via upload

---
+++ ejercicio 16.cpp
@@ -0,0 +1,21 @@
+#include <sys/types.h>
+#include <unistd.h>
+#include <sys/utsname.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <time.h>
+#include <iostream>
+#include <time.h>
+#include <sys/time.h>
+
+using namespace std;
+
+int main(){
+	system("man flock");
+
+ return 1;
+}
+
+
