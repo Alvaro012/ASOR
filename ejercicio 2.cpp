eb36841286858561606591b9ecc8959ca7989415
Alvaro012 <45605035+Alvaro012@users.noreply.github.com>
2021-01-13 18:45:21 +0100

    Update ejercicio 2.cpp

--- ejercicio 2.cpp
+++ ejercicio 2.cpp
@@ -10,20 +10,7 @@
 
 using namespace std;
 
-int main(){
-	struct sched_param param;
-
-        int nice_antes= getpriority(PRIO_PROCESS,0);
-        cout<<"Nice antes: "<< nice_antes<<endl;
-        int nice_nuevo=setpriority(PRIO_PROCESS,0,-10);
-        int nice= getpriority(PRIO_PROCESS,0);
-        cout<<"Nice ahora: "<<nice<<endl;
-
-        int ret = sched_getparam(0,&param);
-        param.sched_priority = 12;
-        int s=sched_setparam(0,&param);
-        int schd=sched_setscheduler(0,SCHED_FIFO,&param);
-     	
+int main(){	
 	int policy=sched_getscheduler(0);
         switch(policy) {
          case SCHED_OTHER: cout<<"SCHED_OTHER"<<endl; break;
