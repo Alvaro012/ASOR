b65d06d38e98daf718e4eaf730d463bee47f0766
Alvaro012 <45605035+Alvaro012@users.noreply.github.com>
2021-01-16 19:53:15 +0100

    Add files via upload

---
+++ ejercicio 3.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<sched.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <sys/utsname.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include<sys/time.h>
+#include<sys/resource.h>
+#include<errno.h>
+#include<fcntl.h>
+#include<signal.h>
+#include<string.h>
+#include<fcntl.h>
+#include<sys/select.h>
+#include<fstream>
+#include<sys/stat.h>
+
+using namespace std;
+
+int main(int argc, char *argv[]){
+        int status;
+        status=mkfifo("/root/fifo.txt",S_IWUSR | S_IRUSR);
+        int stat=system("stat fifo.txt");
+        return 0;
+}
+
+
